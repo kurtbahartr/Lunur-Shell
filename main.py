4d8f14de09e841171375eadd49cb9680cabd9d8f
Diana <diana.ward535@gmail.com>
2025-06-18 21:40:01 -0600

    main.py refactor

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-import subprocess
+import setproctitle
 from fabric import Application
-from fabric.utils import get_relative_path
+from fabric.utils import cooldown, exec_shell_command, get_relative_path
+from loguru import logger
+from gi.repository import Gtk
+
+import utils.functions as helpers
 from modules.bar import StatusBar
 from modules.launcher import AppLauncher
-from services.notifications import NotificationPopup
-from utils import APPLICATION_NAME, widget_config
+from utils import APPLICATION_NAME, ExecutableNotFoundError, widget_config
 
 
-def apply_css(app: Application):
-    result = subprocess.run(
-        ["sass", "styles/main.scss", "dist/main.css", "--no-source-map"],
-        capture_output=True,
-        text=True,
-    )
+@cooldown(2)
+@helpers.run_in_thread
+def process_and_apply_css(app: Application):
+    if not helpers.executable_exists("sass"):
+        raise ExecutableNotFoundError("sass")
 
-    if result.returncode == 0:
+    logger.info("[Main] Compiling CSS")
+    output = exec_shell_command("sass styles/main.scss dist/main.css --no-source-map")
+
+    if output == "":
+        logger.info("[Main] CSS applied")
         app.set_stylesheet_from_file(get_relative_path("dist/main.css"))
-        print("CSS applied")
     else:
         app.set_stylesheet_from_string("")
-        print(f"Failed to compile sass! {result.stderr}")
+        logger.error("[Main] Failed to compile sass!")
 
 
 if __name__ == "__main__":
@@ -29,10 +34,18 @@ if __name__ == "__main__":
     windows = [bar, launcher]
 
     if widget_config.get("notifications", {}).get("enabled", True):
+        from services.notifications import NotificationPopup
         notifications = NotificationPopup(widget_config)
         windows.append(notifications)
 
-    app = Application(APPLICATION_NAME, *windows)
+    app = Application(APPLICATION_NAME, windows=windows)
 
-    apply_css(app)
+    icon_theme = Gtk.IconTheme.get_default()
+    icons_dir = get_relative_path("./assets/icons/svg/gtk")
+    icon_theme.append_search_path(icons_dir)
+
+    process_and_apply_css(app)
+
+    setproctitle.setproctitle(APPLICATION_NAME)
+
     app.run()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from .config import *
 from .constants import *
+from .exceptions import *
 from .thread import *
 from .widget_settings import *
 from .monitors import *
