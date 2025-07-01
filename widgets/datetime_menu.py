8922723fa469ab79e80c35af0de8c7c22ac6015e
Diana <diana.ward535@gmail.com>
2025-07-01 12:32:54 -0600

    date_time use buttonWidget

--- widgets/datetime_menu.py
+++ widgets/datetime_menu.py
@@ -1,29 +1,32 @@
 from fabric.widgets.datetime import DateTime
+from shared import ButtonWidget
 from utils.config import widget_config
 import time
 
-class DateTimeWidget(DateTime):
+class DateTimeWidget(ButtonWidget):
     def __init__(self):
         dt_config = widget_config["date_time"]
 
         date_format = dt_config.get("format", "%b %d")  # e.g. "%b %d"
         clock_format = dt_config.get("clock_format", "24h")
 
-        # Time format depends on clock_format
         time_format = "%I:%M %p" if clock_format == "12h" else "%H:%M"
-
-        # Combine date and time formats
         combined_format = f"{date_format} {time_format}"
-
-        # Use just one format string for simplicity
         formatters = [combined_format]
 
         super().__init__(
+            widget_config["date_time"],
             name="date-time",
+        )
+
+        self.datetime = DateTime(
+            name="inner-date-time",
             formatters=formatters,
         )
 
+        self.box.children = (self.datetime,)
+        self.datetime.show_all()
+
     def do_format(self):
-        # Override to ensure local time is used correctly
-        fmt = self._formatters[self._current_index]
+        fmt = self.datetime._formatters[self.datetime._current_index]
         return time.strftime(fmt, time.localtime())
