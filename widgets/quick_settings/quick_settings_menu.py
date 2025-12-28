26e087018cb6df2fa6a7f2f4ac6dea22fb1f32ed
Diana <diana.ward535@proton.me>
2025-12-28 01:41:46 -0500

    seperate quicksettings components

---
+++ widgets/quick_settings/quick_settings_menu.py
@@ -0,0 +1,55 @@
+# widgets/quick_settings/quick_settings_menu.py
+
+from gi.repository import Gtk
+from fabric.widgets.box import Box
+from fabric.widgets.grid import Grid
+from .sliders.brightness import BrightnessSlider
+from .sliders.volume import VolumeSlider
+from .togglers import NotificationQuickSetting, WifiQuickSetting, BluetoothQuickSetting
+from shared import Popover
+
+
+class SlidersContainer(Box):
+    """Container for brightness and volume sliders."""
+
+    def __init__(self):
+        super().__init__(
+            orientation=Gtk.Orientation.VERTICAL,
+            spacing=8,
+            style_classes="sliders-container",
+        )
+        self.brightness_slider = BrightnessSlider()
+        self.pack_start(self.brightness_slider, False, False, 0)
+        self.volume_slider = VolumeSlider()
+        self.pack_start(self.volume_slider, False, False, 0)
+
+
+class QuickSettingsMenu(Popover):
+    def __init__(self, point_to_widget, config):
+        content_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        content_box.set_name("quick-settings-menu")
+
+        self.sliders = SlidersContainer()
+        content_box.pack_start(self.sliders, False, False, 0)
+
+        separator = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
+        separator.get_style_context().add_class("qs-separator")
+        content_box.pack_start(separator, False, False, 0)
+
+        self.grid = Grid(
+            row_spacing=10,
+            column_spacing=10,
+            column_homogeneous=True,
+            row_homogeneous=True,
+        )
+        self.wifi_btn = WifiQuickSetting()
+        self.bt_btn = BluetoothQuickSetting()
+        self.notification_btn = NotificationQuickSetting()
+
+        self.grid.attach(self.wifi_btn, 0, 0, 1, 1)
+        self.grid.attach(self.bt_btn, 1, 0, 1, 1)
+        self.grid.attach(self.notification_btn, 2, 0, 1, 1)
+
+        content_box.pack_start(self.grid, True, True, 0)
+        content_box.show_all()
+        super().__init__(content=content_box, point_to=point_to_widget)
